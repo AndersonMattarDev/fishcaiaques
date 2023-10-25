2b146e85a963224a5fdf7bd42a670d1c0f7fd256
Anderson Mattar <andmat7745@gmail.com>
2023-10-25 17:31:28 -0300

    alterações em services

--- fishcaiaques/src/components/Services.jsx
+++ fishcaiaques/src/components/Services.jsx
@@ -1,15 +1,35 @@
 import styles from './Services.module.css';
+import imagemCaiaque from '../assets/img7.jpg';
+import trilha47 from '../assets/torre47.jpg'
+import trilhaagua from '../assets/trilha-dagua.jpg'
 
 
 
 export function Services() {
     return(
-       <div className={styles.container}>
-        <h1 data-text = "Nossos_Serviços...">Nossos_Serviços...</h1>
-        <section>serviço 1</section>
-        <section>serviço 2</section>
-        <section>serviço 3</section>        
-       </div>
+        <div className={styles.container}>
+        <h1 data-text="Nossos_Serviços...">Nossos_Serviços...</h1>
+        <div className={styles.cardsContainer}>
+          <section className={styles.card}>
+            <img src={imagemCaiaque} alt="Passeio de caiaque" />
+            <div className={styles.textOverlay}>
+              <p>Texto 1</p>
+            </div>
+          </section>
+          <section className={styles.card}>
+            <img src={trilha47 } alt="Trilha da torre 47" />
+            <div className={styles.textOverlay}>
+              <p>Texto 2</p>
+            </div>
+          </section>
+          <section className={styles.card}>
+            <img src={trilhaagua} alt="Trilha D'agua" />
+            <div className={styles.textOverlay}>
+              <p>Texto 3</p>
+            </div>
+          </section>
+        </div>
+      </div>
        
     ) 
 }
