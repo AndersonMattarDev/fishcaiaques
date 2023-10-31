2f3c0c0e5fb2c2b071b2b55f703b3624d1a8f869
Anderson Mattar <andmat7745@gmail.com>
2023-10-31 17:25:00 -0300

    Avanço em galeria

--- fishcaiaques/src/components/Galeria.jsx
+++ fishcaiaques/src/components/Galeria.jsx
@@ -1,63 +1,86 @@
-import styles from '../components/Galeria.module.css'
-import slide1 from '../assets/img-galery1.jpg'
-import slide2 from '../assets/img-galery2.jpg'
-import slide3 from '../assets/img-galery3.jpg'
-import slide4 from '../assets/img-galery4.jpg'
-import slide5 from '../assets/img-galery5.jpg'
-import slide6 from '../assets/img-galery6.jpg'
+import { useState } from 'react';
+import styles from '../components/Galeria.module.css';
+import { FaStepBackward, FaStepForward } from 'react-icons/fa';
+import slide1 from '../assets/img-galery1.jpg';
+import slide2 from '../assets/img-galery2.jpg';
+import slide3 from '../assets/img-galery3.jpg';
+import slide4 from '../assets/img-galery4.jpg';
+import slide5 from '../assets/img-galery5.jpg';
+import slide6 from '../assets/img-galery6.jpg';
+import slide7 from '../assets/img-galery7.jpg';
+import slide8 from '../assets/img-galery8.jpg';
+import slide9 from '../assets/img-galery9.jpg';
+import slide10 from '../assets/img-galery10.jpeg';
+import slide11 from '../assets/img-galery11.jpeg';
+import slide12 from '../assets/img-galery12.jpg';
 
-export function Galeria(){
+const imageWidth = 250; // Largura de cada imagem
+const gap = 15; // Espaçamento entre imagens
 
-  return(
-    <div className={styles.slider}>
+export function Galeria() {
+  const [currentItem, setCurrentItem] = useState(0);
 
-      <div className={styles.slides}>
-        <input type="radio" name="radio-btn" id="radio1" />
-        <input type="radio" name="radio-btn" id="radio2" />
-        <input type="radio" name="radio-btn" id="radio3" />
-        <input type="radio" name="radio-btn" id="radio4" />
-        <input type="radio" name="radio-btn" id="radio5" />
-        <input type="radio" name="radio-btn" id="radio6" />
-      </div>
+  const items = [
+    slide1,
+    slide2,
+    slide3,
+    slide4,
+    slide5,
+    slide6,
+    slide7,
+    slide8,
+    slide9,
+    slide10,
+    slide11,
+    slide12,
+  ];
 
-      <div className={styles.slide}>
-        <img src={slide1} alt="slide1" />
-      </div>
-      <div className={styles.slide}>
-        <img src={slide2} alt="slide2" />
-      </div>
-      <div className={styles.slide}>
-        <img src={slide3} alt="slide3" />
-      </div>
-      <div className={styles.slide}>
-        <img src={slide4} alt="slide4" />
-      </div>
-      <div className={styles.slide}>
-        <img src={slide5} alt="slide5" />
-      </div>
-      <div className={styles.slide}>
-        <img src={slide6} alt="slide6" />
-      </div>
+  const handlePrevious = () => {
+    setCurrentItem((prevItem) => (prevItem - 1 + items.length) % items.length);
+  };
 
-      <div className={styles.naviAuto}>
-        <div className={styles.autoBtn1}></div>
-        <div className={styles.autoBtn2}></div>
-        <div className={styles.autoBtn3}></div>
-        <div className={styles.autoBtn4}></div>
-        <div className={styles.autoBtn5}></div>
-        <div className={styles.autoBtn6}></div>
-      </div>
+  const handleNext = () => {
+    setCurrentItem((prevItem) => (prevItem + 1) % items.length);
+  };
 
-      <div className={styles.manualNav}>
-        <label htmlFor="radio1" className={styles.manualBtn}></label>
-        <label htmlFor="radio2" className={styles.manualBtn}></label>
-        <label htmlFor="radio2" className={styles.manualBtn}></label>
-        <label htmlFor="radio3" className={styles.manualBtn}></label>
-        <label htmlFor="radio4" className={styles.manualBtn}></label>
-        <label htmlFor="radio5" className={styles.manualBtn}></label>
-        <label htmlFor="radio6" className={styles.manualBtn}></label>
-      </div>
+  const transformX = -(currentItem * (imageWidth + gap));
 
+  return (
+    <div className={styles.container}>
+      <h1>Galeria de Aventuras</h1>
+      <button
+        className={`${styles.arrowLeft} ${styles.control}`}
+        aria-label="Previous Image"
+        onClick={handlePrevious}
+      >
+        <FaStepBackward />
+      </button>
+
+      <button
+        className={`${styles.arrowRight} ${styles.control}`}
+        aria-label="Next Image"
+        onClick={handleNext}
+      >
+        <FaStepForward />
+      </button>
+
+      <div className={styles.galleryWrapper}>
+        <div
+          className={styles.gallery}
+          style={{ transform: `translateX(${transformX}px)` }}
+        >
+          {items.map((item, index) => (
+            <img
+              key={index}
+              src={item}
+              alt={`slide ${index + 1}`}
+              className={`${styles.item} ${
+                index === currentItem ? styles.currentItem : ''
+              }`}
+            />
+          ))}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
